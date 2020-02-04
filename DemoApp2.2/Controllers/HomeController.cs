52d36a52e4d86d2487aff8cb45cd6a142d68504c
JAYRAJ DATTA <jayrajdatta@gmail.com>
2020-02-04 23:55:38 +0530

    add new dotnet 2.2 core mvc app

---
+++ DemoApp2.2/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using DemoApp2._2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoApp2._2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public string Index()
+        {
+            //return "Hello from Home Controller";
+            return _employeeRepository.GetEmployee(1).Name;
+        }
+
+        //public JsonResult Index()
+        //{
+        //    return Json(new { id=1, name="Jayraj"});
+        //}
+
+        //public JsonResult Details()
+        //{
+        //    Employee empModel = _employeeRepository.GetEmployee(1);
+        //    return Json(empModel);
+        //}
+
+        //public ObjectResult Details()
+        //{
+        //    Employee empModel = _employeeRepository.GetEmployee(1);
+        //    return new ObjectResult(empModel);
+        //}
+
+        public ViewResult Details()
+        {
+            Employee empModel = _employeeRepository.GetEmployee(1);
+            ViewData["Employee"] = empModel;
+            ViewData["PageTitle"] = "Employee Detais";
+
+            return View(empModel);
+            //return View();
+        }
+    }
+}
---
+++ DemoApp2.2/Models/MockEmployeeRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoApp2._2.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee(){Id=1, Name="Amar", Department="HR", Email="amar@test.com"},
+                new Employee(){Id=1, Name="Samar", Department="IT", Email="samar@test.com"},
+                new Employee(){Id=1, Name="Alok", Department="IT", Email="alok@test.com"}
+            };
+        }
+        public Employee GetEmployee(int id)
+        {
+            return _employeeList.FirstOrDefault<Employee>(e => e.Id == id);
+        }
+    }
+}
